d9c715ee9f06e33d48b0c9b0ca3f0a957eec1570
Irfan Ul Haq <iamiq07@gmail.com>
2026-07-13 22:44:19 +0530

    selection sort

---
+++ selection.java
@@ -0,0 +1,30 @@
+class Selection{
+    public static void main(String args[])
+    {
+        int i,j,min,ind,swap;
+        int[] a = {34,21,67,48,93};
+        for(i=0;i<3;i++)
+         {
+            ind = i;
+            min = a[i];
+            for(j=1;j<3;j++)
+            {
+                if(a[j]<min)
+                {
+                    min = a[j];
+                    ind = j;
+                }
+            }
+            if(a[i] != min)
+            {
+                swap = a[i];
+                a[i] = a[ind];
+                a[ind] = swap;
+            }
+         }
+         for(i=0;i<5;i++)
+         {
+            System.out.println(a[i]);
+         }
+    }
+}
